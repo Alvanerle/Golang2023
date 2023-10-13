5e052b22be4352799c218dc61010ae5bfaeb66e7
Alvanerle <zzhumash03@gmail.com>
2023-10-13 19:23:25 +0600

    Chapter 4 finished (Assignment 2 finished)

--- Assignment2/cmd/api/printers.go
+++ Assignment2/cmd/api/printers.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"Printers.imangalizhumash.net/internal/data"
+	"Printers.imangalizhumash.net/internal/validator"
 	"fmt"
 	"net/http"
 	"time"
 )
 
 func (app *application) createPrinterHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new Printer")
+	var input struct {
+		Name                string       `json:"name"`
+		Type                string       `json:"type"`
+		IsColor             bool         `json:"is_color"`
+		Status              string       `json:"status"`
+		Description         string       `json:"description"`
+		SupportedPaperSizes []string     `json:"supported_paper_sizes"`
+		BatteryLeft         data.Runtime `json:"battery_left"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	printer := &data.Printer{
+		Name:                input.Name,
+		Type:                input.Type,
+		IsColor:             input.IsColor,
+		Status:              input.Status,
+		Description:         input.Description,
+		SupportedPaperSizes: input.SupportedPaperSizes,
+		BatteryLeft:         input.BatteryLeft,
+	}
+
+	v := validator.New()
+
+	if data.ValidatePrinter(v, printer); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showPrinterHandler(w http.ResponseWriter, r *http.Request) {
--- Assignment2/internal/data/printers.go
+++ Assignment2/internal/data/printers.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"Printers.imangalizhumash.net/internal/validator"
+	"time"
+)
 
 type Printer struct {
 	ID                  int64     `json:"id"`
@@ -13,4 +16,15 @@ type Printer struct {
 	SupportedPaperSizes []string  `json:"supported_paper_sizes"`
 	Description         string    `json:"description,omitempty"`
 	BatteryLeft         Runtime   `json:"battery_left,omitempty"` // -1 means not chargeable
+}
+
+func ValidatePrinter(v *validator.Validator, printer *Printer) {
+	v.Check(printer.Name != "", "name", "must be provided")
+	v.Check(len(printer.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	v.Check(printer.BatteryLeft != 0, "battery_left", "must be provided")
+
+	v.Check(printer.SupportedPaperSizes != nil, "supported_paper_sizes", "must be provided")
+	v.Check(len(printer.SupportedPaperSizes) > 0, "supported_paper_sizes", "must contain at least 1 supported paper size")
+	v.Check(validator.Unique(printer.SupportedPaperSizes), "supported_paper_sizes", "must not contain duplicate values")
 }
